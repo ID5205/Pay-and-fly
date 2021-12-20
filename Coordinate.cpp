2a89ad3307563fc998f902e9b497f3cfe55ccfee
Александр Караваев <93885806+AleksandrKaravaev@users.noreply.github.com>
2021-12-20 15:39:14 +0300

    Add files via upload

---
+++ Coordinate.cpp
@@ -0,0 +1,30 @@
+
+#include "Coordinate.h"
+Coordinate::Coordinate()
+{
+    x = 0;
+    y = 0;
+    z = 0;
+}
+Coordinate::Coordinate(float new_x, float new_y, float new_z)
+{
+	x = new_x;
+	y = new_y;
+	z = new_z;
+}
+void Coordinate::set_xyz(float new_x, float new_y, float new_z)
+{
+	x = new_x;
+	y = new_y;
+	z = new_z;
+}
+void Coordinate::add_xyz(float d_x, float d_y, float d_z)
+{
+	x += d_x;
+	y += d_y;
+	z += d_z;
+}
+float Coordinate::get_x() {return x;}
+float Coordinate::get_y() {return y;}
+float Coordinate::get_z() {return z;}
+
---
+++ Coordinate.h
@@ -0,0 +1,20 @@
+#ifndef COORDINATE
+#define COORDINATE
+
+
+class Coordinate
+{
+	float x;
+	float y;
+	float z;
+public:
+    Coordinate();
+	Coordinate(float new_x, float new_y, float new_z);
+	void set_xyz(float new_x, float new_y, float new_z);
+	void add_xyz(float d_x, float d_y, float d_z);
+	float get_x();
+	float get_y();
+	float get_z();
+};
+
+#endif //COORDINATE
